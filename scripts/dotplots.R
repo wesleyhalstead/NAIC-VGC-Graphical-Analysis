a5d0609258cd6bf6d468ce698ab2e43dc4151efa
Wesley Halstead <wesleyhalstead@sbcglobal.net>
2025-07-14 22:32:54 -0500

    dotcharts now produced in base R

--- scripts/dotplots.R
+++ scripts/dotplots.R
@@ -30,11 +30,10 @@ pdf(file = 'plots/dotchart_usage.pdf',
 dotchart(as.numeric(rev(t1)),
          labels = nt1,
          cex = 0.65, pt.cex = 1,
-         pch = 1)
-abline(v = (0:5)*50, lty = 2,  col = rgb(1,0,0, alpha = 0.6),
-       lwd = 1.5)
-abline(v = (0:4)*50+25, lty = 2, col = rgb(1,0,0, alpha = 0.2),
-       lwd = 1.5)
+         pch = 20)
+  grid(nx = NULL, ny = NULL)
+  
+  points(as.numeric(rev(t1)), pch = 20, 1:86)
 }
 dev.off()
 
@@ -69,6 +68,7 @@ restrictedDuosPlacement <- restrictedDuosPlacement %>% arrange((medPlacement))
 
 restrictedDuosPlacement <- restrictedDuosPlacement %>% arrange((highestPlacement))
 
+
 {
   dotchart(x= rev(pull(restrictedDuosPlacement, highestPlacement)),
            labels =  rev(pull(restrictedDuosPlacement, restrictedDuo)),
@@ -81,6 +81,31 @@ restrictedDuosPlacement <- restrictedDuosPlacement %>% arrange((highestPlacement
 }
 
 
+
+#### Attempting to put multiple summaries with dotchart
+temp1 <- as.data.frame(restrictedDuosPlacement)
+
+pdf(file = 'plots/dotchart_placement.pdf',
+    height = 11,
+    width = 8.5,
+    pointsize = 12)
+{
+dotchart(x= rev(temp1$highestPlacement),
+         labels =  rev(temp1$restrictedDuo),
+         cex = 0.65, pt.cex = 1,
+         pch = 20, lcolor = rgb(0,0,0,0))
+
+grid(nx = NULL, ny = 87)
+
+points(temp1$avgPlacement,86:1, col = '#00bf7d', pch = 20)
+points(temp1$medPlacement,86:1, col = '#5928ed', pch = 20)
+points(temp1$highestPlacement,86:1, pch = 20)
+
+legend(1,8, legend = c('Highest Placement','Median Placement','Mean Placement'),
+       cex = 0.65, fill = c('black','#5928ed','#00bf7d'))
+}
+dev.off()
+
 ### Trying to produce multiple placement summaries on same plot
 temp1 <- restrictedDuosPlacement %>% pivot_longer(
   !restrictedDuo
@@ -90,10 +115,10 @@ sortingOrder <- rep(restrictedDuosPlacement$highestPlacement, each = 3)
 
 library(lattice)
 
-pdf(file = 'plots/dotchart_placement.pdf',
+"pdf(file = 'plots/dotchart_placement.pdf',
     height = 11,
     width = 8.5,
-    pointsize = 12)
+    pointsize = 12)"
 {
 dotplot(reorder(restrictedDuo,-sortingOrder)~value, data=temp1,
         groups = name,
@@ -109,4 +134,4 @@ dotplot(reorder(restrictedDuo,-sortingOrder)~value, data=temp1,
                  )
         )
 }
-dev.off()
+#dev.off()
